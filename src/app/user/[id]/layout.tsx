41574d7ad1ccf74e153bc2e463b35ac387262674
Ketchupchh <80965149+Ketchupchh@users.noreply.github.com>
2023-07-01 20:36:38 -0500

    'use client' fix

--- src/app/user/[id]/layout.tsx
+++ src/app/user/[id]/layout.tsx
@@ -1,11 +1,4 @@
-'use client'
-
-import { Placeholder } from '@/components/common/placeholder';
-import { UserContextProvider } from '@/lib/context/user-context';
-import { usersCollection } from '@/lib/firebase/collections';
-import { useCollection } from '@/lib/hooks/useCollection';
-import { limit, query, where } from 'firebase/firestore';
-import { useParams } from 'next/navigation'
+import { UserDataLayout } from '@/components/layout/user-data-layout';
 
 export const metadata = {
   title: 'Instagram',
@@ -17,25 +10,9 @@ export default function UserLayout({
 }: {
   children: React.ReactNode
 }) {
-
-    const { id } = useParams();
-
-    const { data, loading } = useCollection(
-      query(usersCollection, where('username', '==', id), limit(1)),
-      { allowNull: true }
-    );
-
-    const user = data ? data[0] : null;
-
-    return (
-      <UserContextProvider value={{ user, loading }}>
-        {loading ? (
-            <Placeholder />
-        ) : (
-            <>
-                {children}
-            </>
-        )}
-      </UserContextProvider>
-    );
+  return (
+    <UserDataLayout>
+      {children}
+    </UserDataLayout>
+  );
 }
